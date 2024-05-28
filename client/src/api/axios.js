e8348f2527d8fb7a3fc57e008082b7ea6bf82c8f
Jared Graham <jaredgraham@jareds-mbp.lan>
2024-05-27 22:38:42 -0400

    Update environment variable to VITE_PRODUCTION_URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 // const BASE_URL = process.env.NGROK_API;
 
-const BASE_URL = process.env.PRODUCTION_URL;
+const BASE_URL = import.meta.env.VITE_PRODUCTION_URL;
 console.log("API Base URL:", BASE_URL);
 export default axios.create({
   baseURL: BASE_URL,
